4ee2ce932528f7dddbc4a9e151b5ab3130fe16f6
Suren06 <suren.boyilla@gmail.com>
2024-04-14 15:23:26 +0800

    Add a middileware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || !/^\S+@\S+\.\S+$/.test(email)) {
-      return res.status(400).json({ message: "Invalid or missing fields." });
+      next(errorHandler(400, "All fields are required"));
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
@@ -17,7 +18,6 @@ export const signup = async (req, res) => {
 
     res.status(201).json({ message: "User created successfully!" });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error creating user" }); // Handle specific errors better
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import { error } from "console";
 
 dotenv.config();
 
@@ -25,3 +26,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
