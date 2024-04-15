4e0cb45c78419c5238e4ca1346d968b3de477204
Suren06 <suren.boyilla@gmail.com>
2024-04-15 09:10:49 +0800

    Add Redux toolkit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,27 +2,25 @@ import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
 import {
   BsFacebook,
-  BsTwitter,
   BsInstagram,
-  BsLinkedin,
-  BsGoogle,
+  BsTwitter,
   BsGithub,
+  BsDribbble,
 } from "react-icons/bs";
-
-const FooterCom = () => {
+export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
-          <div className="mt-5 ">
+          <div className="mt-5">
             <Link
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
-              <span className="px-2 py-1 rounded-lg bg-gradient-to-r from-teal-500 via-sky-500 to-green-500 text-white">
-                Spires
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Surens
               </span>
-              Reads
+              Blog
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
@@ -30,18 +28,18 @@ const FooterCom = () => {
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://www.100projects.com"
+                  href="https://www.100jsprojects.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Spire Reads
+                  100 JS Projects
                 </Footer.Link>
                 <Footer.Link
-                  href="https://www.100projects.com"
+                  href="/about"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Spire Reads
+                  Surens Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -49,38 +47,20 @@ const FooterCom = () => {
               <Footer.Title title="Follow us" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://github.com/Suren06"
+                  href="https://www.github.com/sahandghavidel"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Spire Reads
+                  Github
                 </Footer.Link>
-                <Footer.Link
-                  href="https://github.com/Suren06"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Spire Reads
-                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Leagal" />
+              <Footer.Title title="Legal" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://github.com/Suren06"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Privacy policy
-                </Footer.Link>
-                <Footer.Link
-                  href="https://github.com/Suren06"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Spire Reads
-                </Footer.Link>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -89,21 +69,21 @@ const FooterCom = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
-            by="Suren's blog"
-            year={new Date().getFullYear}
+            by="Sahand's blog"
+            year={new Date().getFullYear()}
           />
-          <div className="flex gap-6 sm-mt-0 mt-4 sm:justify-center">
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsLinkedin} />
-            <Footer.Icon href="#" icon={BsGoogle} />
-            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon
+              href="https://github.com/sahandghavidel"
+              icon={BsGithub}
+            />
+            <Footer.Icon href="#" icon={BsDribbble} />
           </div>
         </div>
       </div>
     </Footer>
   );
-};
-
-export default FooterCom;
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,11 +21,10 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields");
+      return dispatch(signInFailure("Please fill out all fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -27,16 +32,14 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-
-      setLoading(false);
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
 
