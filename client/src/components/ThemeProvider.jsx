7f1233a2b026e08eda0be9d45b42c74cb4611e17
Suren06 <suren.boyilla@gmail.com>
2024-04-23 09:53:54 +0800

    Made the dashboard page private

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,8 +5,9 @@ const ThemeProvider = ({ children }) => {
   const { theme } = useSelector((state) => state.theme);
   return (
     <div className={theme}>
-      <div className="bg-white text-gray-200 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen"></div>
-      {children}
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen">
+        {children}
+      </div>
     </div>
   );
 };
